5d961c2fb3b01fe66fff3e8612ec51bb3d40ed50
Kish-Siva-1 <rodes88@msn.com>
2019-08-01 14:27:10 +0200

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(somevalue){
   // Write your code here!
+
+  if (somevalue <= 400) {
+    return 'This one is on me!';
+  } else if (somevalue > 2000 && somevalue < 2499) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (somevalue > 2500) {
+    return 'No can do.';
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  switch (tip) {
+    case 'generous':
+        return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.'
+    case 'thanks for everything':
+      return 'Bye.';
+  }
+
 }
